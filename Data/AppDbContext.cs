9871b5be3732c4704e621b8b7a4ea30cde2af3c1
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-10-18 20:52:34 -0300

    created migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<TodoModel> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
--- teste.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TodoApp;
-
-public class teste
-{
-    
-}
