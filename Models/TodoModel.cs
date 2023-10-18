9ada7ed6e21e839de76deb49510b5856b505b941
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-10-18 20:29:42 -0300

    First commit

---
+++ Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.Models;
+
+public class TodoModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Done { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ teste.cs
@@ -0,0 +1,6 @@
+﻿namespace TodoApp;
+
+public class teste
+{
+    
+}
