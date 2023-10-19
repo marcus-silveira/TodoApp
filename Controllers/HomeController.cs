1bc86166f23c7579975c0f8070fa2bdfd1d6a3d4
Marcus Silveira <marcusvinicius.767.mv@gmaiil.com>
2023-10-18 21:35:45 -0300

    Criado primeiros get e post piloto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet("/")]
+    public List<TodoModel> Get([FromServices] AppDbContext appDbContext)
+    {
+        return appDbContext.Todos.ToList();
+    }
+    
+    [HttpPost("/")]
+    public List<TodoModel> Post([FromServices] AppDbContext appDbContext, [FromBody] TodoModel model)
+    {
+        var todo = new TodoModel()
+        {
+            Title = model.Title,
+            Done = model.Done,
+            CreatedAt = DateTime.Now
+        };
+
+        appDbContext.Todos.Add(todo);
+        appDbContext.SaveChanges();
+        return appDbContext.Todos.ToList();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using TodoApp.Data;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
+
+var app = builder.Build();
+app.MapControllers();
 
 app.Run();
